6747e9c62c54aada1ff57b4e134eb780293afeeb
hanyxp <hanyxp@163.com>
2020-10-23 16:56:12 +0800

    init

---
+++ public/test.php
@@ -0,0 +1,27 @@
+<?php
+namespace han\test;
+use ReflectionClass;
+use ReflectionMethod;
+class A {
+    public function __construct() {
+        echo __CLASS__;
+    }
+
+    public function __init() {
+        echo 'aaaaaa';
+    }
+    
+}
+
+class B extends A {
+    public function __construct() {
+        echo 'class B init';
+        parent::__construct();
+    }
+}
+
+$b = new B();
+$c = new \ReflectionClass(B::class);
+//var_dump($c);
+var_dump($c->getMethods(ReflectionMethod::IS_PUBLIC));
+?>
